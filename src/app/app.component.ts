ff61cbc9d0c48fdb4b541cbdf2b927c1e74e5b25
Delyan Ninov <dninov@gmail.com>
2022-01-08 08:17:46 +0200

    Custom Error and Validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,10 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl(null, Validators.required),
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNames.bind(this), // dont forget to bind(this)
+        ]),
         email: new FormControl(null, [Validators.required, Validators.email]),
       }),
       gender: new FormControl("male"),
@@ -27,5 +30,13 @@ export class AppComponent implements OnInit {
   }
   onSubmit() {
     console.log(this.signupForm);
+  }
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return { nameIsForbidden: true };
+    }
+    return null;
+    // should not be false!
   }
 }
